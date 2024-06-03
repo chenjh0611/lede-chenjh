b5436f84a4914a2d2a6c5da73d71d9726d135bbd
chen.jh <chenjh0611@163.com>
2024-06-03 17:32:36 +0800

    一些个性修改

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -181,8 +181,10 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio0.ssid=WFSZ-2.4G
+			set wireless.default_radio1.ssid=WFSZ-5G
+			set wireless.default_radio${devidx}.encryption=psk2
+			set wireless.default_radio${devidx}.key=QWERDF123...
 EOF
 		uci -q commit wireless
 
